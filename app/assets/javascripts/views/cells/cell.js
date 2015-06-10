ee7fd74b90a01a0fabeae8bcc798bad922ff395d
App Academy Student <austin.e.gatlin@gmail.com>
2015-06-10 10:17:59 -0700

    seeds mines, css's cells, cell turn red if you hit a mine

--- app/assets/javascripts/views/cells/cell.js
+++ app/assets/javascripts/views/cells/cell.js
@@ -1,5 +1,9 @@
 MinesweeperLeague.Views.Cell = Backbone.View.extend({
 
+  initialize: function () {
+    this.mined = this._seedMine(0.2);
+  },
+
   attributes: {
     "class": "cell",
   },
@@ -13,5 +17,21 @@ MinesweeperLeague.Views.Cell = Backbone.View.extend({
   },
 
   template: JST['cells/cell'],
+
+  events: {
+    'click': 'reveal'
+  },
+
+  reveal: function (event) {
+    this.revealed = true;
+    this.$el.addClass('revealed');
+    if (this.mined) {
+      this.$el.addClass('mined');
+    }
+  },
+
+  _seedMine: function (fraction) {
+    return Math.random() <= fraction ? true : false;
+  },
 
 });
