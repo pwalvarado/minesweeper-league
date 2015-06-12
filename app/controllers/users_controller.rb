32389c82eef394d56f8c870e8ca262a6b1a2cf96
App Academy Student <austin.e.gatlin@gmail.com>
2015-06-12 08:59:08 -0700

    users log in automatically on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in(@user)
       redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
