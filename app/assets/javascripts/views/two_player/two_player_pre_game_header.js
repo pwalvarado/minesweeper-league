671f3a871644e302a3e3dbf3ec2d9ed81d6a7064
App Academy Student <austin.e.gatlin@gmail.com>
2015-06-20 00:06:27 -0700

    only allows players to hit the start button when at least two players have joined

--- app/assets/javascripts/views/two_player/two_player_pre_game_header.js
+++ app/assets/javascripts/views/two_player/two_player_pre_game_header.js
@@ -4,19 +4,8 @@ MinesweeperLeague.Views.TwoPlayerPreGameHeader = Backbone.View.extend({
     this.pusher = options.pusher;
     this.channel = options.channel;
 
-    this.channel.bind('pusher:member_added', function () {
-      if (this.channel.members.count < 2) {
-        this.$el.find('.start-btn').addClass('btn-warning').addClass('disable')
-          .removeClass('.btn-success').html('Waiting for an opponent...');
-      }
-    });
-
-    this.channel.bind('pusher:subscription_successful', function () {
-      if (this.channel.members.count < 2) {
-        this.$el.find('.start-btn').addClass('btn-warning').addClass('disable')
-          .removeClass('.btn-success').html('Waiting for an opponent...');
-      }
-    });
+    this.channel.bind('pusher:member_added', this.render, this);
+    this.channel.bind('pusher:subscription_succeeded', this.render, this);
   },
 
   className: 'two-player-pre-game-header-row row',
@@ -24,8 +13,22 @@ MinesweeperLeague.Views.TwoPlayerPreGameHeader = Backbone.View.extend({
   template: JST['two_player/pre_game_header'],
 
   render: function () {
+    debugger;
     this.$el.html(this.template());
+    this.channel.members.count > 1 ? this.enableButton() : this.disableButton();
 
     return this;
+  },
+
+  disableButton: function () {
+    this.$el.find('.start-btn').removeClass('.btn-success')
+      .addClass('btn-warning').addClass('disabled')
+      .html('Waiting for an opponent...');
+  },
+
+  enableButton: function () {
+    this.$el.find('.start-btn').removeClass('btn-warning')
+      .removeClass('disabled').addClass('btn-success')
+      .html('Start');
   },
 });
