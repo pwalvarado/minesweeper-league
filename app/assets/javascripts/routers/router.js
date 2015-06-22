e7a63f43888357d91c293a2fb1e4897d04c4a7db
App Academy Student <austin.e.gatlin@gmail.com>
2015-06-21 21:46:12 -0700

    fixes navbar active bug

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -13,8 +13,17 @@ MinesweeperLeague.Routers.Router = Backbone.Router.extend({
   main: function () {
     var mainView = new MinesweeperLeague.Views.Main();
 
-    $('.navbar-nav').not('.navbar-right').children().removeClass('active');
-    $(".nav > li").has("a[href='#']").addClass('active');
+    if ($(".main div:contains('Log In')").length) {
+      $('.navbar-nav').children().removeClass('active');
+      $(".nav > li").has(":contains('Log In')").addClass('active');
+    } else if ($(".main div:contains('Sign Up')")) {
+      $('.navbar-nav').children().removeClass('active');
+      $(".nav > li").has(":contains('Sign Up')").addClass('active');
+    } else {
+      $('.navbar-nav').children().removeClass('active');
+      $(".nav > li").has("a[href='#']").addClass('active');
+    }
+
     this._swapView(mainView);
   },
 
@@ -34,7 +43,7 @@ MinesweeperLeague.Routers.Router = Backbone.Router.extend({
       gameId: gameId
     });
 
-    $('.navbar-nav').not('.navbar-right').children().removeClass('active');
+    $('.navbar-nav').children().removeClass('active');
     $(".nav > li").has("a[href='#two_player_games/new']").addClass('active');
     this._swapView(twoPlayerView);
   },
